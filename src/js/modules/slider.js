8e84497674c64ebaf5599f5913034f3e20501c6e
Lev <tyanlev1989@gmail.com>
2022-05-08 16:56:53 +0500

    026_Всплывающий_со_временем_блок

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -11,6 +11,20 @@ export class Slider {
 
     if (n < 1) this.slideIndex = this.slides.length;
 
+    try {
+      this.hanson.style.opacity = '0';
+
+      if (n === 3) {
+        this.hanson.classList.add('animated');
+        setTimeout(() => {
+          this.hanson.style.opacity = '1';
+          this.hanson.classList.add('slideInUp');
+        }, 3000);
+      } else {
+        this.hanson.classList.remove('slideInUp');
+      }
+    } catch (e) {}
+
     this.slides.forEach((slide) => (slide.style.display = 'none'));
 
     this.slides[this.slideIndex - 1].style.display = 'block';
@@ -21,6 +35,10 @@ export class Slider {
   }
 
   render() {
+    try {
+      this.hanson = document.querySelector('.hanson');
+    } catch (e) {}
+
     this.btns.forEach((btn) => {
       btn.addEventListener('click', () => this.plusSlides(1));
 
